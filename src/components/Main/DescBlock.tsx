9a07e333bfd19d74e1c7929972a27fbd9397d4d3
Orest Hlavatskyi <_>
2025-03-12 15:32:33 +0200

    Hot-fix with height of descblock

--- src/components/Main/DescBlock.tsx
+++ src/components/Main/DescBlock.tsx
@@ -13,11 +13,11 @@ const DescBlock = ({
   altForImage,
 }: DescProps) => {
   return (
-    <div className="h-[291px] md:w-[354px] rounded-4xl bg-conic from-angular-gradient-1  via-angular-gradient-3 to-angular-gradient-6 flex justify-center items-center">
+    <div className="h-[303px] md:h-[291px] md:w-[354px] rounded-4xl bg-conic from-angular-gradient-1  via-angular-gradient-3 to-angular-gradient-6 flex justify-center items-center">
       <div className="w-[98%] h-[98%] rounded-4xl p-[40px] bg-black-bg">
         <img src={imgPath} alt={altForImage} className="mb-[20px]" />
         <h3 className="font-poppins-semibold text-[18px] mb-[8px]">{title}</h3>
-        <p className="font-poppins-light text-[14px]">
+        <p className="font-poppins-light text-[14px] leading-[20px]">
           {description.split(/(\s|\,|\.)+/).map((word) => {
             if (strong.split(/(\s|\,|\.)+/).includes(word)) {
               return <strong key={word}>{word} </strong>;
