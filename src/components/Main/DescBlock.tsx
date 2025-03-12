18d6ee8cccb29d62332c8c171ab05bfdc738711f
Orest Hlavatskyi <_>
2025-03-12 15:23:21 +0200

    Fixed border in DescBlock

--- src/components/Main/DescBlock.tsx
+++ src/components/Main/DescBlock.tsx
@@ -13,8 +13,8 @@ const DescBlock = ({
   altForImage,
 }: DescProps) => {
   return (
-    <div className="flex-1/3 rounded-4xl bg-conic from-angular-gradient-1  via-angular-gradient-3 to-angular-gradient-6 flex justify-center items-center">
-      <div className="w-[99%] h-[93%] md:h-[98%] rounded-4xl p-[40px] bg-black-bg">
+    <div className="h-[291px] md:w-[354px] rounded-4xl bg-conic from-angular-gradient-1  via-angular-gradient-3 to-angular-gradient-6 flex justify-center items-center">
+      <div className="w-[98%] h-[98%] rounded-4xl p-[40px] bg-black-bg">
         <img src={imgPath} alt={altForImage} className="mb-[20px]" />
         <h3 className="font-poppins-semibold text-[18px] mb-[8px]">{title}</h3>
         <p className="font-poppins-light text-[14px]">
